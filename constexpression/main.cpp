c989a6ac8d919500703120ab1b5b6b037fb1bd29
Ferhat Erata <ferhat.erata@yale.edu>
2019-12-28 23:57:17 -0500

    constexpr is added from cppreference.com

---
+++ constexpression/main.cpp
@@ -0,0 +1,5 @@
+//  ----------------------------------------------------------------------------
+//  Header file for the main class.        main.cpp
+//  Created by Ferhat Erata <ferhat.erata@yale.edu> on December 28, 2019.
+//  Copyright (c) 2019 Yale University. All rights reserved.
+// -----------------------------------------------------------------------------
