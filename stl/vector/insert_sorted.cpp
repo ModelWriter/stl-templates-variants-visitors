d025ce7e2702734538769c06353812bae2b421c3
Ferhat Erata <ferhat.erata@yale.edu>
2020-08-14 23:43:04 -0400

    reformatted

--- stl/vector/insert_sorted.cpp
+++ stl/vector/insert_sorted.cpp
@@ -7,8 +7,14 @@
 #include "../stl.hpp"
 #include <algorithm>
 #include <cassert>
+#include <iostream>
 #include <string>
 #include <vector>
+
+void insert_sorted(std::vector<std::string>& v, const std::string& word) {
+    const auto insert_pos(lower_bound(begin(v), end(v), word));
+    v.insert(insert_pos, word);
+}
 
 int main() {
     std::vector<std::string> v{"some",  "random", "words", "without",
@@ -16,4 +22,12 @@ int main() {
     assert(!my::is_sorted(begin(v), end(v)));
     std::sort(begin(v), end(v));
     assert(std::is_sorted(begin(v), end(v)));
+
+    insert_sorted(v, "foobar");
+    insert_sorted(v, "zzz");
+
+    for (const auto& w : v) {
+        std::cout << w << " ";
+    }
+    std::cout << '\n';
 }
